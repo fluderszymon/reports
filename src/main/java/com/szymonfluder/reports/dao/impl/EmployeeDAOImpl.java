d322ed3a74ef334b39472001e2dad2e759d65a08
fluderszymon <szymon.fluder@outlook.com>
2025-04-25 20:19:19 +0200

    Refactor employee

--- src/main/java/com/szymonfluder/reports/dao/impl/EmployeeDAOImpl.java
+++ src/main/java/com/szymonfluder/reports/dao/impl/EmployeeDAOImpl.java
@@ -2,8 +2,6 @@ package com.szymonfluder.reports.dao.impl;
 
 import com.szymonfluder.reports.entity.Employee;
 import com.szymonfluder.reports.dao.EmployeeDAO;
-import com.szymonfluder.reports.dto.EmployeeDTO;
-import com.szymonfluder.reports.mapper.EmployeeMapper;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
@@ -12,48 +10,42 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
 public class EmployeeDAOImpl implements EmployeeDAO {
 
     private final EntityManager entityManager;
-    private final EmployeeMapper employeeMapper;
+
+
 
     @Autowired
-    public EmployeeDAOImpl(EntityManager entityManager, EmployeeMapper employeeMapper) {
+    public EmployeeDAOImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
-        this.employeeMapper = employeeMapper;
     }
 
     @Override
     @Transactional
-    public void addEmployee(EmployeeDTO employeeDTO) {
-        Employee employee = employeeMapper.employeeDtoToEmployee(employeeDTO);
+    public void addEmployee(Employee employee) {
         Query query = entityManager.createQuery("INSERT INTO Employee (firstName, lastName, email) " +
                                                 "VALUES (:firstName, :lastName, :email)")
             .setParameter("firstName", employee.getFirstName())
             .setParameter("lastName", employee.getLastName())
             .setParameter("email", employee.getEmail());
-
         query.executeUpdate();
     }
 
     @Override
-    public EmployeeDTO getEmployeeById(int id) {
+    public Employee getEmployeeById(int id) {
         TypedQuery<Employee> query = entityManager.createQuery("SELECT u FROM Employee u " +
                                                                 "WHERE u.id=:id", Employee.class)
             .setParameter("id", id);
-        return employeeMapper.employeeToEmployeeDto(query.getSingleResult());
+        return query.getSingleResult();
     }
 
     @Override
-    public List<EmployeeDTO> getAllEmployees() {
+    public List<Employee> getAllEmployees() {
         TypedQuery<Employee> query = entityManager.createQuery("FROM Employee", Employee.class);
-        return query.getResultList()
-                .stream()
-                .map(employeeMapper::employeeToEmployeeDto)
-                .collect(Collectors.toList());
+        return query.getResultList();
     }
 
     @Override
@@ -61,14 +53,12 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     public void deleteEmployeeById(int id) {
         Query query = entityManager.createQuery("DELETE FROM Employee e WHERE e.id=:id")
             .setParameter("id", id);
-
         query.executeUpdate();
     }
 
     @Override
     @Transactional
-    public void updateEmployee(EmployeeDTO employeeDTO) {
-        Employee employee = employeeMapper.employeeDtoToEmployee(employeeDTO);
+    public void updateEmployee(Employee employee) {
         Query query = entityManager.createQuery("UPDATE Employee " +
                         "SET firstName =:firstName, lastName =:lastName, email =:email " +
                         "WHERE id=:id")
@@ -76,7 +66,6 @@ public class EmployeeDAOImpl implements EmployeeDAO {
                 .setParameter("lastName", employee.getLastName())
                 .setParameter("email", employee.getEmail())
                 .setParameter("id", employee.getId());
-
         query.executeUpdate();
     }
 }
